94a18af3700aeda67f35955128070222144e35d3
sivaguru003 <Sivaguru.Sivakumar@infinite.com>
2026-04-08 11:53:22 +0530

    Update Prgm4.cs

--- Assessments/C#/Ass.Code2/Ass.Code2/Prgm4.cs
+++ Assessments/C#/Ass.Code2/Ass.Code2/Prgm4.cs
@@ -1,43 +1,43 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//internal class Prgm4
-//{
-//    delegate int Calculator(int a, int b);
-//    static int Add(int x, int y)
-//    {
-//        return x + y;
-//    }
-//    static int Sub(int x, int y)
-//    {
-//        return x - y;
-//    }
-//    static int Mul(int x, int y)
-//    {
-//        return x * y;
-//    }
-//    static void Calculate(int a, int b, Calculator cal)
-//    {
-//        int res = cal(a, b);
-//        Console.WriteLine(res);
-//    }
-//    public static void calc()
-//    {
-//        Console.Write("Enter first number: ");
-//        int num1 = int.Parse(Console.ReadLine());
-//        Console.Write("Enter second number: ");
-//        int num2 = int.Parse(Console.ReadLine());
-//        Console.Write("Addition : ");
-//        Calculate(num1, num2, Add);
-//        Console.Write("Subtraction : ");
-//        Calculate(num1, num2, Sub);
-//        Console.Write("Multiplication : ");
-//        Calculate(num1, num2, Mul);
-//        Console.ReadLine();
-//    }
-//}
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class Prgm4
+{
+   delegate int Calculator(int a, int b);
+   static int Add(int x, int y)
+   {
+       return x + y;
+   }
+   static int Sub(int x, int y)
+   {
+       return x - y;
+   }
+   static int Mul(int x, int y)
+   {
+       return x * y;
+   }
+   static void Calculate(int a, int b, Calculator cal)
+   {
+       int res = cal(a, b);
+       Console.WriteLine(res);
+   }
+   public static void calc()
+   {
+       Console.Write("Enter first number: ");
+       int num1 = int.Parse(Console.ReadLine());
+       Console.Write("Enter second number: ");
+       int num2 = int.Parse(Console.ReadLine());
+       Console.Write("Addition : ");
+       Calculate(num1, num2, Add);
+       Console.Write("Subtraction : ");
+       Calculate(num1, num2, Sub);
+       Console.Write("Multiplication : ");
+       Calculate(num1, num2, Mul);
+       Console.ReadLine();
+   }
+}
 
 
 
