8afde8f1045f60cd05c8b2f111686cc976b826be
sivaguru003 <Sivaguru.Sivakumar@infinite.com>
2026-04-08 11:53:00 +0530

    Update Prgm3.cs

--- Assessments/C#/Ass.Code2/Ass.Code2/Prgm3.cs
+++ Assessments/C#/Ass.Code2/Ass.Code2/Prgm3.cs
@@ -1,37 +1,37 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//internal class Prgm3
-//{
-//    static void Checknumber(int num)
-//    {
-//        if (num < 0)
-//        {
-//            throw new Exception("Number is negative");
-//        }
-//        else
-//        {
-//            Console.WriteLine("Number is Positive : " + num);
-//        }
-//    }
-//    public static void chknum()
-//    {
-//        try
-//        {
-//            Console.Write("Enter a Number: ");
-//            int number = int.Parse(Console.ReadLine());
+internal class Prgm3
+{
+   static void Checknumber(int num)
+   {
+       if (num < 0)
+       {
+           throw new Exception("Number is negative");
+       }
+       else
+       {
+           Console.WriteLine("Number is Positive : " + num);
+       }
+   }
+   public static void chknum()
+   {
+       try
+       {
+           Console.Write("Enter a Number: ");
+           int number = int.Parse(Console.ReadLine());
 
-//            Checknumber(number);
-//        }
-//        catch (Exception ex)
-//        {
-//            Console.WriteLine(ex.Message);
-//        }
-//        Console.ReadLine();
-//    }
+           Checknumber(number);
+       }
+       catch (Exception ex)
+       {
+           Console.WriteLine(ex.Message);
+       }
+       Console.ReadLine();
+   }
 
-//}
+}
 
