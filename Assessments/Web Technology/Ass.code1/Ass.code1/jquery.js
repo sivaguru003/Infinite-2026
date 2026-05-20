e14ec4cfc2c0396e933234e32c6560482992a069
Sivaguru Sivakumar <sivagurus@infinite.com>
2026-05-20 13:39:26 +0530

    Assessments

---
+++ Assessments/Web Technology/Ass.code1/Ass.code1/jquery.js
@@ -0,0 +1,11 @@
+﻿<style>
+    table, td {
+        border: 1px solid black;
+    border-collapse: collapse;
+    padding: 5px;
+        }
+
+    .color {
+        background - color: yellow;
+        }
+</style>
